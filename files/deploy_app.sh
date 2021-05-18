4160781eb146caeba0ed61103376d1dc2cd9dcb3
Anwar Haque <emailhaque@gmail.com>
2021-05-18 22:10:30 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Testing Anwar Haque May.
+  Welcome to ${PREFIX}'s app. Testing Anwar Haque May 2021.
   <!-- END -->
 
   </div>
